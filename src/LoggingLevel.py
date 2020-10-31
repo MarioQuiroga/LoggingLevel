067ce662895e7b07a7402cec30670f55c89334b3
MarioQuiroga <marioquiro17@gmail.com>
2020-10-31 00:39:02 -0300

    output file fixed

--- src/LoggingLevel.py
+++ src/LoggingLevel.py
@@ -1,26 +1,31 @@
 import os
 class Logging:
   def __init__(self, level: str='LOG'):
-    ''' Create Logging Level 
-    :level: Set current level
+    ''' Create Logging Level .
+    :level: Set current level.
     '''    
     self.level = level
     self.file_output = ''
 
-  def log(self, log:str, file:str='---', func:str='---'):
-    ''' Print log if env == curr_level
-    :log: message to print
+  def log(self, string:str, file:str='---', func:str='---'):
+    ''' Print log if env == level.
+    :string: message to print.
     '''
     env = os.getenv('env', '')
     if self.level == env or env == '':
-      text_log = '['+ self.level + '] ['+ file  +'] ['+ func + '] '+ log
+      text_log = '['+ self.level + '] ['+ file  +'] ['+ func + '] '+ string
       print(text_log)      
       if self.file_output != '':
-        with open(file_output, 'a'):
-          write(text_log)
+        with open(self.file_output, 'a') as f:
+          f.write(text_log+'\n')
 
-  def set_file_output(self, file:str):
-    self.file = file
+  def set_output_file(self, file:str):
+    ''' If it was not configured, the log will not be saved in a file
+    '''
+    self.file_output = file
 
   def set_level(self, level:str):
+    ''' Change current level.
+    :level: any text can be used. 
+    '''
     self.level = level
